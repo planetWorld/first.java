318e86518ef8b1794f2170be136e70cbd385fcef
planetWorld <32408347+planetWorld@users.noreply.github.com>
2018-10-07 14:45:51 +0900

    Update file01.java

--- calender/file01.java
+++ calender/file01.java
@@ -1,6 +1,31 @@
 import java.util.*;
 
-public class file01 {
+public class file01{
+   public static void file01 (String[]args){
+      Scanner sc =new Scanner(System.in);
+        int line =sc.next();
+        System.out.println(int);
+      int[]number ={1,2,3,4,5,6,7,8,9,10,11,12};
+        System.out.println(number[1] + "月");  
+        System.out.println(number[2] + "月"); 
+        System.out.println(number[3] + "月");  
+        System.out.println(number[4] + "月"); 
+        System.out.println(number[5] + "月");  
+        System.out.println(number[6] + "月"); 
+        System.out.println(number[7] + "月");  
+        System.out.println(number[8] + "月"); 
+        System.out.println(number[9] + "月");  
+        System.out.println(number[10] + "月"); 
+        System.out.println(number[11] + "月");  
+        System.out.println(number[12] + "月"); 
+     
+     
+     
+     
+          
+        
+   }
+}
 
 
 
